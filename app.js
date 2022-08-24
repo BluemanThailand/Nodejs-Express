80a7f4496107661902f2f2bb2c249df4e6c6fac9
BluemanThailand <84426686+BluemanThailand@users.noreply.github.com>
2022-08-24 23:50:00 +0700

    add productsRouter

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const products = require("./data/products.json");
-const productRouter =express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")))
@@ -14,20 +13,8 @@ app.use(express.static(path.join(__dirname, "/public/")))
 app.set("views", "./rec/views");
 app.set("view engine", "ejs")
 
-productRouter.route("/").get((req, res) => {
-    res.render("products",{
-        products,  
-});
-});
 
-productRouter.route("/:id").get((req, res) => {
-    const id = req.params.id;
-    res.render("product",{
-        product: products[id],
-    })
-});
-
-app.use("/products", productRouter)
+app.use("/products", productsRouter)
 
 app.get("/", (req,res) =>{
 
